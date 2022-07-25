7e4422007f9c012a6e3dc64d0835645e714c9bd9
JoseEspinosaTello <jet5588@outlook.com>
2022-07-25 03:00:55 -0500

    update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,8 +6,32 @@ console.log("working");
 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker to the map for Los Angeles, California.
+
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//change to circle
+
+// L.circle([34.0522, -118.2437], {
+//     radius: 100,
+//     color: 'black',
+//     fillColor: '#ffffa1',
+//     fillOpacity: 0.5,
+// }).addTo(map);
+
+//add the circle marker
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//replace - https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}
+//with dark-v10 - this give dark background even outside a dark mode browser
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',

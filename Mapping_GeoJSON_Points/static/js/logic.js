638c54f7f48d0f01ea48deb27895d72a0590a271
JoseEspinosaTello <jet5588@outlook.com>
2022-08-25 14:47:55 -0500

    update

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -8,21 +8,59 @@ console.log("working");
 //let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375, 37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
-  
+// Grabbing our GeoJSON data.
+//L.geoJSON(sanFranAirport).addTo(map);
+
+
+L.geoJSON(sanFranAirport, {
+	// We turn each feature into a marker on the map.
+	onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+  }
+
+}).addTo(map);
+
+// add marker with functionality
+// Grabbing our GeoJSON data.
+
+L.geoJSON(sanFranAirport, {
+
+
+	pointToLayer: function(feature, latlng) {
+	
+		console.log(feature);
+		
+		return L.marker(latlng)
+    
+    //add the popup marker
+    .bindPopup("<h2>" + feature.properties.city + "</h2>");
+		
+	}
+	
+}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.

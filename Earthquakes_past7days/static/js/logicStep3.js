30ae2a6f49a69bcd6aa93568682c73ea0871587d
JoseEspinosaTello <jet5588@outlook.com>
2022-08-26 14:38:33 -0500

    update

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -26,7 +26,7 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
-    layers: [streets]
+    layers: [satelliteStreets]
 })
 
 // This function determines the radius of the earthquake marker based on its magnitude.
@@ -51,25 +51,76 @@ function getRadius(magnitude) {
 
 // to calculate the radius.
 
-function styleInfo(feature) {
+// This function returns the style data for each of the earthquakes we plot on
 
-	return {
+// the map. We pass the magnitude of the earthquake into two separate functions
+
+// to calculate the color and radius.
+
+function styleInfo(feature) { 
+
+	return { 
 		
 		opacity: 1,
 		
 		fillOpacity: 1,
 		
-		fillColor: "#ffae42",
+		fillColor: getColor(feature.properties.mag),
 		
 		color: "#000000",
 		
-		radius: getRadius(),
+		radius: getRadius(feature.properties.mag),
 		
 		stroke: true,
 		
 		weight: 0.5
 		
 	};
+	
+}
+
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+
+function getColor (magnitude) { 
+
+	if (magnitude > 5) {
+	
+		return "#ea2c2c";
+		
+	}
+	
+	if (magnitude > 4) {
+	
+		return "#ea822c";
+		
+	}
+	
+	if (magnitude > 3) {
+	
+		return "#ee9c00";
+		
+	}
+	
+	if (magnitude > 2) {
+	
+		return "#eecc00";
+		
+	}
+	
+	if (magnitude > 2) {
+	
+		return "#eecc00";
+		
+	}
+	
+	if (magnitude > 1) { 
+	
+		return "#d4ee00";
+		
+	}
+	
+	return"#98ee00";
 	
 }
 
@@ -95,6 +146,17 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
 				// We set the style for each circleMarker using our styleInfo function.
 				style: styleInfo,
-			
+
+					// We create a popup for each circleMarker to display the magnitude and
+		
+					//  location of the earthquake after the marker has been created and styled.
+		
+					onEachFeature: function(feature, layer) {
+		
+					layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+	
+				}
+
 			}).addTo(map);
+		
 });
